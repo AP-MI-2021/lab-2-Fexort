ac9cab29ed2137e46ae5364c3d17ae254b5ec573
unknown <opreamatei82@ymail.com>
2021-10-06 19:06:19 +0300

    al 2 lea commit

--- main.py
+++ main.py
@@ -37,12 +37,41 @@ def get_base_2(n):
         n=n//2
     return m
 
+'''
+Găsește ultimul număr prim mai mic decât un număr dat.
+'''
+def get_largest_prime_below(n):
+
+    maxprim = -1
 
 
+    while n % 2 == 0:
+        maxprim = 2
+        n = n / 2
+
+    while n % 3 == 0:
+        maxprim = 3
+        n = n / 3
+
+    for i in range(5, int(n//2) + 1):
+        while n % i == 0:
+            maxprim = i
+            n = n // i
+        while n % (i + 2) == 0:
+            maxprim = i + 2
+            n = n // (i + 2)
+
+    return (maxprim)
+def test_get_largest_prime_below():
+    n = 15
+    print(get_largest_prime_below(n))
 
 def main():
 
+    # interfata de tip consola aici
 
+    if __name__ == '__main__':
+        main()
 
 
 
